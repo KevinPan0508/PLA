731feb3165102934869a174b50dfb7f9e742bb7a
KevinPan0508 <k0922322268@gmail.com>
2021-01-22 22:35:50 +0800

    pla practice

---
+++ pla.py
@@ -0,0 +1,34 @@
+from dataset import np, dataset
+import matplotlib.pyplot as plt
+
+def pla(dataset):
+    w = np.zeros(3)
+    while(check_error(w,dataset) is not None):
+        x, s = check_error(w,dataset)
+        w = w + s*x
+        ps = [v[0] for v in dataset]
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+
+        ax1.scatter([v[1] for v in ps[:4]], [v[2] for v in ps[:4]], s=10, c='b', marker="o", label='O')
+        ax1.scatter([v[1] for v in ps[4:]], [v[2] for v in ps[4:]], s=10, c='r', marker="x", label='X')
+        l = np.linspace(-2,2)
+        a,b = -w[1]/w[2], -w[0]/w[2]
+        ax1.plot(l, a*l + b, 'b-')
+        plt.legend(loc='upper left')
+        plt.show()
+    return w
+
+def check_error(w,dataset):
+    result = None
+    error = 0
+    for x, s in dataset:
+        x = np.array(x)
+        if(int(np.sign(w.dot(x)))!= s):
+            result = x, s
+            error = error + 1
+    print("error=%s/%s"%(error, len(dataset)))
+    return result
+
+
+
